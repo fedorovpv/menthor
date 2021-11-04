c5b0f87397840220cb86efb213cb642ed77466f7
Pav.fedorov@gmail.com <Lu1tKrie6>
2021-11-04 04:58:35 +0300

    test model

--- LoanDecider.py
+++ LoanDecider.py
@@ -2,6 +2,7 @@ import math
 
 
 class LoanDecider:
+    """Class for loan decision"""
     BASE_RATE = 10
 
     INC_SRC_UNEMPLOYED = 'безработный'
@@ -43,66 +44,82 @@ class LoanDecider:
         OBJ_CONSUMER_LOAN: 1.5
     }
 
-    def __init__(self, age, sex, income_src, income_year, rating, asking_for, time, objective):
+    def __init__(self, age, sex, income_src, income_year, rating, asking_for, years, objective):
+        """Initialization"""
         self.age = age
         self.sex = sex
         self.income_src = income_src
         self.income_year = income_year
         self.rating = rating
         self.asking_for = asking_for
-        self.time = time
+        self.years = years
         self.objective = objective
 
     @property
     def is_not_retired(self):
+        """Checking is person retired according to russian law"""
         return ((self.sex == self.SEX_FEMALE) and (self.age < 60)) or\
                ((self.sex == self.SEX_MALE) and (self.age < 65))
 
     @property
     def is_income_ok(self):
-        return self.asking_for / self.time <= self.income_year / 3
+        """Checking that (desired amount / (time in years)) is less than third of year income."""
+        return self.asking_for / self.years <= self.income_year / 3
 
     @property
     def is_rating_ok(self):
+        """Checking rating is OK"""
         return self.rating > -2
 
     @property
     def is_source_income_ok(self):
+        """Checking that source income is appropriate."""
         return self.income_src != self.INC_SRC_UNEMPLOYED
 
-    @property
-    def year_payment_is_ok(self):
-        return self.income_year / 2 >= self.year_payment
+    def year_payment_is_ok(self, year_payment):
+        """Checking that year payment is less than half of year income."""
+        return self.income_year / 2 >= year_payment
 
     @property
     def max_amount(self):
+        """Getting max amount from desired and acceptable."""
         return min(self.MAX_AMOUNT_PER_INCOME_SOURCE[self.income_src], self.MAX_AMOUNT_PER_RATING[self.rating])
 
     @property
     def modifier_by_ammount(self):
+        """Getting modifier for amount"""
         return -math.log(self.approved_amount, 10)
 
     @property
     def rate_modifiers(self):
+        """Getting total modifiers."""
         return self.MODIFIERS_PER_OBJ[self.objective] + self.MODIFIERS_PER_INCOME_SOURCE[self.income_src]\
                + self.modifier_by_ammount
 
     @property
     def year_payment(self):
-        return self.approved_amount * (1 + self.time * self.rate) / self.time
+        """Getting year payment"""
+        return self.approved_amount * (1 + self.years * self.rate / 100) / self.years
 
     @property
     def approved_amount(self):
+        """Getting approved amount from max amount and desirable one."""
         return min(self.asking_for, self.max_amount)
 
     @property
     def rate(self):
+        """Getting rate from base and particular part."""
         return self.BASE_RATE + self.rate_modifiers
 
     @property
-    def approved(self):
-        return self.is_not_retired and self.is_income_ok and self.is_rating_ok and self.is_source_income_ok and\
-               self.year_payment_is_ok
+    def preapproved(self):
+        """Base checks for approve"""
+        return self.is_not_retired and self.is_income_ok and self.is_rating_ok and self.is_source_income_ok
 
     def decide(self):
-        return self.approved, self.year_payment
+        """Main method to decide to be or not to be."""
+        if self.preapproved:
+            year_payment = self.year_payment
+            if self.year_payment_is_ok(year_payment):
+                return True, year_payment
+        return False, None
---
+++ tests/LoanDeciderTests.py
@@ -0,0 +1,105 @@
+import pytest
+import math
+from LoanDecider import LoanDecider
+
+
+def expected_year_amount(amount, years, rate):
+    """Auxiliary function to count year amount;"""
+    return amount * (1 + years * (rate - math.log(amount, 10)) / 100) / years
+
+
+class TestLoanDecider:
+    """Tests for Loan Decider"""
+    @staticmethod
+    def get_config(age=20, sex=LoanDecider.SEX_MALE, income_src=LoanDecider.INC_SRC_OWN_BUSINESS,
+                   income_year=100, rating=2, asking_for=1, years=1, objective=LoanDecider.OBJ_CAR_LOAN):
+        """
+        Getting base config for ordinary man with the most simple characteristics for loan. By  default, objective
+         is car loan for 0 modifier, rating and income are at max, term of loan is 1 for simplifying. Rate is Base 10
+         plus 0.25 for 'own business'
+        """
+        return {'age': age, 'sex': sex, 'income_src': income_src, 'income_year': income_year, 'rating': rating,
+                'asking_for': asking_for, 'years': years, 'objective': objective}
+
+    @pytest.mark.parametrize(
+        'message, exp_approved, params', [
+            ('Basic config, test should be successful for the precision of further tests.', True, {}),
+            ('Unemployed person.', False, {'income_src': LoanDecider.INC_SRC_UNEMPLOYED}),
+            ('Male retired.', False, {'age': 65}),
+            ('Male almost retired.', True, {'age': 64}),
+            ('Female retired.', False, {'age': 60, 'sex': LoanDecider.SEX_FEMALE}),
+            ('Female almost retired.', True, {'age': 59, 'sex': LoanDecider.SEX_FEMALE}),
+            ('Low rating.', False, {'rating': -2}),
+            ('Rating is OK: -1.', True, {'rating': -1}),
+            ('Rating is OK: 0.', True, {'rating': 0}),
+            ('Rating is OK: 1.', True, {'rating': 1}),
+            ('Rating is OK: 2.', True,  {'rating': 2}),
+            ('Amount without percentage is huge, but still OK. Asking_for / years == year_income / 3.', True,
+             {'asking_for': 1, 'years': 1, 'income_year': 3}),
+            ('Amount without percentage is huge. Asking_for / years > year_income / 3.', False,
+             {'asking_for': 1.0001, 'years': 1, 'income_year': 3})
+        ])
+    def test_approve(self, message, exp_approved, params):
+        """Testing approval for loan"""
+        config = self.get_config(**params)
+        loan_decider = LoanDecider(**config)
+        approved, year_payment = loan_decider.decide()
+        if exp_approved:
+            year_payment_is_ok = year_payment is not None
+        else:
+            year_payment_is_ok = year_payment is None
+
+        assert (exp_approved == approved) and year_payment_is_ok,\
+            message + f"\nExpected approve: {exp_approved}, actual: {approved};\n" \
+                      f"Year payment: {year_payment}.\nConfig: {config}"
+
+    @pytest.mark.parametrize(
+        'message, exp_year_payment, params', [
+            ('Passive income', expected_year_amount(amount=1, years=1, rate=10.5),
+             {'income_src': LoanDecider.INC_SRC_PASSIVE, 'asking_for': LoanDecider.MAX_AMOUNT_PER_INCOME_SOURCE[LoanDecider.INC_SRC_PASSIVE]}),
+
+            ('Hired income,', expected_year_amount(amount=5, years=1, rate=9.75),
+             {'income_src': LoanDecider.INC_SRC_HIRED, 'asking_for': LoanDecider.MAX_AMOUNT_PER_INCOME_SOURCE[LoanDecider.INC_SRC_HIRED]}),
+
+            ('Own business income', expected_year_amount(amount=10, years=1, rate=10.25),
+             {'income_src': LoanDecider.INC_SRC_OWN_BUSINESS, 'asking_for': LoanDecider.MAX_AMOUNT_PER_INCOME_SOURCE[LoanDecider.INC_SRC_OWN_BUSINESS]}),
+
+            ('Rating -1', expected_year_amount(amount=1, years=1, rate=10.25), {'rating': -1, 'asking_for': LoanDecider.MAX_AMOUNT_PER_RATING[-1]}),
+            ('Rating 0', expected_year_amount(amount=5, years=1, rate=10.25), {'rating': 0, 'asking_for': LoanDecider.MAX_AMOUNT_PER_RATING[0]}),
+            ('Rating 1', expected_year_amount(amount=10, years=1, rate=10.25), {'rating': 1, 'asking_for': LoanDecider.MAX_AMOUNT_PER_RATING[1]}),
+            ('Rating 2', expected_year_amount(amount=10, years=1, rate=10.25), {'rating': 2, 'asking_for': LoanDecider.MAX_AMOUNT_PER_RATING[2]})
+        ])
+    def test_border_values_max_amount(self, message, exp_year_payment, params):
+        """Testing border values"""
+        config = self.get_config(**params)
+        loan_decider = LoanDecider(**config)
+        approved, year_payment = loan_decider.decide()
+        assert approved and (exp_year_payment == year_payment),\
+            message + f" Max amount but still OK. \nExpected approve: True, actual: {approved};\n" \
+                      f"Expected year payment: {exp_year_payment}, actual: {year_payment}.\nConfig: {config}"
+
+        config['asking_for'] += 0.01
+        loan_decider = LoanDecider(**config)
+        approved, year_payment = loan_decider.decide()
+        assert approved and (exp_year_payment == year_payment), \
+            message + f"More than max amount\nExpected approve: True, actual: {approved};\n" \
+                      f"Expected year payment: {exp_year_payment}, actual: {year_payment}.\nConfig: {config}"
+
+    @pytest.mark.parametrize(
+        'message, exp_year_payment, params', [
+            ('Modifiers changing. Objective: Car Loan', expected_year_amount(amount=1, years=1, rate=10.25), {}),
+            ('Modifiers changing. Objective: Mortgage', expected_year_amount(amount=1, years=1, rate=8.25), {'objective': LoanDecider.OBJ_MORTGAGE}),
+            ('Modifiers changing. Objective: Business Improvement', expected_year_amount(amount=1, years=1, rate=9.75), {'objective': LoanDecider.OBJ_BUSINESS_IMPROVEMENT}),
+            ('Modifiers changing. Objective: Consumer Loan', expected_year_amount(amount=1, years=1, rate=11.75), {'objective': LoanDecider.OBJ_CONSUMER_LOAN}),
+            ('Modifiers changing. Small amount', expected_year_amount(amount=0.1, years=1, rate=10.25), {'asking_for': 0.1}),
+            ('Modifiers changing. Huge amount', expected_year_amount(amount=10, years=1, rate=10.25), {'asking_for': 10}),
+            ('Time changes year payment as expected', expected_year_amount(amount=1, years=20, rate=10.25), {'years': 20}),
+        ])
+    def test_modifiers(self, message, exp_year_payment, params):
+        """Testing year amount."""
+        config = self.get_config(**params)
+        loan_decider = LoanDecider(**config)
+        approved, year_payment = loan_decider.decide()
+        assert approved and (exp_year_payment == year_payment), \
+            message + f"\nExpected approve: True, actual: {approved};\n" \
+                      f"Expected year payment: {exp_year_payment}, actual: {year_payment}.\nConfig: {config}"
